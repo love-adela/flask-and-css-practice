cf26cb299e9a74a6bd70a25c392a3b250e22472d
love-adelar <love.adelar@gmail.com>
2020-11-03 23:37:29 +0900

    title 넘겨줌

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template(
+        'index.html',
+        title = '아델라의 블로그'
+        )
+
+# TODO: POST 방식으로 아티클 생성 요청
+# @app.route('/articles', methods=['POST'])
+# def post_article():
+#     url_receive = request.form['']
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
