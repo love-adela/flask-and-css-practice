57c761d370a33a98e18db86fbdb09d4efe403923
love-adelar <love.adelar@gmail.com>
2020-11-04 21:54:04 +0900

    ajax로 넘겨서 데이터 받음

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template(
-        'index.html',
-        title = '아델라의 블로그',
-        post_list = ['논리회로에서 음수 표현하기', '알고리즘 개념 잡기', '비둘기집의 원리'],
-        post_subtitle = 'MSB의 값에 따라 양수와 음수를 표현할 수 있다.',
-        post_date = ['2020-11-03',],
-        tag_list = ['#computer_science', '#algorithm']
-        )
+    return render_template('index.html')
 
-# TODO: POST 방식으로 아티클 생성 요청
-# @app.route('/articles', methods=['POST'])
-# def post_article():
-#     url_receive = request.form['']
+@app.route('/article', methods=['GET'])
+def get_articles():
+    # TODO: 1. 모든 docuemtn 찾기 & _id 값은 출력에서 제외하기
+    # TODO: 2. articles라는 키 값으로 영화정보 내려주기
+    return jsonify({'result':'success', 'msg':'GET 연결되었습니다!'})
+
+@app.route('/articles', methods=['POST'])
+def post_articles():
+    # TODO: 1.클라이언트로부터 데이터 받기
+    title = request.form['title']
+    subtitle = request.form['subtitle']
+    tags = request.form['tags']
+    
+    article = {'title': title, 'subtitle': subtitle, 'tags': tags}
+    # TODO: 2.mongoDB에 데이터 넣기
+    
+    return jsonify({'result': 'success', 'msg': 'POST 연결되었습니다!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
