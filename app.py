f3c3e1d68e1bfbc269732c9a35f6238d6d41ab4a
love-adelar <love.adelar@gmail.com>
2020-11-04 14:40:38 +0900

    db에 넣기 전에 일단 화면에 뿌리기 용으로 임시로 데이터 넣음

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 def index():
     return render_template(
         'index.html',
-        title = '아델라의 블로그'
+        title = '아델라의 블로그',
+        post_list = ['논리회로에서 음수 표현하기', '알고리즘 개념 잡기', '비둘기집의 원리'],
+        post_subtitle = 'MSB의 값에 따라 양수와 음수를 표현할 수 있다.',
+        post_date = ['2020-11-03',],
+        tag_list = ['#computer_science', '#algorithm']
         )
 
 # TODO: POST 방식으로 아티클 생성 요청
