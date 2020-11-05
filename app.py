caac68b302a24507db49c82caab7eef01d3b8ddb
love-adelar <love.adelar@gmail.com>
2020-11-06 00:12:50 +0900

    테스트용으로 넣어두었던 msg 뺌.

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def index():
 @app.route('/article', methods=['GET'])
 def get_articles():
     result = list(db.articles.find({}, {'_id':0}))
-    return jsonify({'result':'success', 'msg':'GET 연결되었습니다!', 'articles': result})
+    return jsonify({'result':'success', 'articles': result})
 
 @app.route('/article', methods=['POST'])
 def post_articles():
