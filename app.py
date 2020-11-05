e9bdda3a9bd8dd863ff0d673728f278dd946a69c
love-adelar <love.adelar@gmail.com>
2020-11-05 16:46:57 +0900

    DB에 데이터 저장

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, jsonify, request
-from pymongo import MongoClient
-
 app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbBlog
 
 @app.route('/')
 def index():
@@ -9,14 +11,14 @@ def index():
 
 @app.route('/article', methods=['GET'])
 def get_articles():
-    # TODO: 1. 모든 docuemtn 찾기 & _id 값은 출력에서 제외하기
-    # TODO: 2. articles라는 키 값으로 영화정보 내려주기
-    return jsonify({'result':'success', 'msg':'GET 연결되었습니다!'})
+    result = list(db.articles.find({}, {'_id':0}))
+    return jsonify({'result':'success', 'msg':'GET 연결되었습니다!', 'articles': result})
 
 @app.route('/article', methods=['POST'])
 def post_articles():
-    data = request.get_json()
-    return jsonify({'result':'success', 'result_data': data})
+    article = request.get_json()
+    db.articles.insert_one(article)
+    return jsonify({'result':'success'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
